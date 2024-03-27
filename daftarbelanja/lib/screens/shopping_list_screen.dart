0e5ce9d402a23458e56eef323909ddfbd09df295
indraw26 <126231069+indraw26@users.noreply.github.com>
2024-03-27 08:50:27 +0700

    add Delete Function on item
    .

--- daftarbelanja/lib/screens/shopping_list_screen.dart
+++ daftarbelanja/lib/screens/shopping_list_screen.dart
@@ -37,10 +37,40 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
                     _shoppingService.addShoppingItem(_controller.text);
                     _controller.clear();
                   },
-                )
+                ),
               ],
             ),
-          )
+          ),
+          // Children kedua
+          Expanded(
+              child: StreamBuilder<Map<String, String>>(
+            stream: _shoppingService.getShoppingList(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                Map<String, String> items = snapshot.data!;
+                return ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    final key = items.keys.elementAt(index);
+                    final item = items[key];
+                    return ListTile(
+                      title: Text(item!),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _shoppingService.removeShoppingItem(key);
+                        },
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text("Error: ${snapshot.error}"));
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ))
         ],
       ),
     );
